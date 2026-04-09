eb18c46ba2432e7fc5ce534e51e5abbdd841d874
ANTU SHIL <anturoy084@gmail.com>
2026-04-09 21:55:55 +0600

    added page button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Next Food</h1>
+    <div className="flex flex-col gap-5 min-h-screen justify-center text-center items-stretch">
+      <div className="relative">
+        <h1 className="text-4xl font-bold">Next JS Fetching & Rendering</h1>
+        <h2 className="text-8xl absolute inset-0 -top-20 opacity-10 font-medium">Fetchoing Rendering & Styling</h2>
+      </div>
+      <div className="">
+        <button className="btn">Lets Explore</button>
+      </div>
     </div>
   );
 }

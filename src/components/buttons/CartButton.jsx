5c9c1fe58e55e89e16db7a79e8ab6be267ec2fc9
ANTU SHIL <anturoy084@gmail.com>
2026-05-01 20:37:59 +0600

    added context pages

--- src/components/buttons/CartButton.jsx
+++ src/components/buttons/CartButton.jsx
@@ -14,7 +14,7 @@ const CartButton = ({food}) => {
     <button
       onClick={handleadd2Cart}
       disabled={inCart}
-      className="flex-1 bg-green-600 text-white py-2 rounded-lg disabled:bg-amber-600 hover:bg-green-700 transition"
+      className="flex-1 bg-green-600 text-white py-3 rounded-lg disabled:bg-amber-600 hover:bg-green-700 transition"
     >
       {inCart ? "Added " : "Add to Cart"}
     </button>
